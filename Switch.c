17a172980b598cb0abf2ae152b98498e1ebaf791
aznfrosting <alice94@sbcglobal.net>
2016-09-18 20:54:56 -0500

    Switch update?

--- Switch.c
+++ Switch.c
@@ -1,147 +1,53 @@
-// Switch.c
-// Runs on LM4F120/TM4C123
-// Provide functions that initialize a GPIO as an input pin and
-// allow reading of two negative logic switches on PF0 and PF4
-// and an external switch on PA5.
-// Use bit-banded I/O.
-// Daniel and Jonathan Valvano
-// September 12, 2013
-
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
-#include "SysTick.h"
-
-#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
-#define PF0                     (*((volatile uint32_t *)0x40025004))
-#define PF4                     (*((volatile uint32_t *)0x40025040))
-#define PA5                     (*((volatile uint32_t *)0x40004080))
-#define SWITCHES                (*((volatile uint32_t *)0x40025044))
-#define SW1       0x10                      // on the left side of the Launchpad board
-#define SW2       0x01                      // on the right side of the Launchpad board
-#define SYSCTL_RCGC2_GPIOF      0x00000020  // port F Clock Gating Control
-
-//------------Switch_Init------------
-// Initialize GPIO Port A bit 5 for input
-// Input: none
-// Output: none
-void Switch_Init(void){ 
-  SysTick_Init();
-  SYSCTL_RCGCGPIO_R |= 0x00000001;     // 1) activate clock for Port A
-  while((SYSCTL_PRGPIO_R&0x01) == 0){};// ready?
-  GPIO_PORTA_AMSEL_R &= ~0x20;      // 3) disable analog on PA5
-  GPIO_PORTA_PCTL_R &= ~0x00F00000; // 4) PCTL GPIO on PA5
-  GPIO_PORTA_DIR_R &= ~0x20;        // 5) direction PA5 input
-  GPIO_PORTA_AFSEL_R &= ~0x20;      // 6) PA5 regular port function
-  GPIO_PORTA_DEN_R |= 0x20;         // 7) enable PA5 digital port
-}
-//------------Switch_Input------------
-// Read and return the status of GPIO Port A bit 5 
-// Input: none
-// Output: 0x20 if PA5 is high
-//         0x00 if PA5 is low
-uint32_t Switch_Input(void){
-  return PA5; // return 0x20(pressed) or 0(not pressed)
-}
-uint32_t Switch_Input2(void){
-  return (GPIO_PORTA_DATA_R&0x20); // 0x20(pressed) or 0(not pressed)
-}
-
-//------------Board_Init------------
-// Initialize GPIO Port F for negative logic switches on PF0 and
-// PF4 as the Launchpad is wired.  Weak internal pull-up
-// resistors are enabled, and the NMI functionality on PF0 is
-// disabled.
-// Input: none
-// Output: none
-void Board_Init(void){            
-  SYSCTL_RCGCGPIO_R |= 0x20;     // 1) activate Port F
-  while((SYSCTL_PRGPIO_R&0x20) == 0){};// ready?
-                                 // 2a) unlock GPIO Port F Commit Register
-  GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
-  GPIO_PORTF_CR_R |= (SW1|SW2);  // 2b) enable commit for PF4 and PF0
-                                 // 3) disable analog functionality on PF4 and PF0
-  GPIO_PORTF_AMSEL_R &= ~(SW1|SW2);
-                                 // 4) configure PF0 and PF4 as GPIO
-  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFF0FFF0)+0x00000000;
-  GPIO_PORTF_DIR_R &= ~(SW1|SW2);// 5) make PF0 and PF4 in (built-in buttons)
-                                 // 6) disable alt funct on PF0 and PF4
-  GPIO_PORTF_AFSEL_R &= ~(SW1|SW2);
-//  delay = SYSCTL_RCGC2_R;        // put a delay here if you are seeing erroneous NMI
-  GPIO_PORTF_PUR_R |= (SW1|SW2); // enable weak pull-up on PF0 and PF4
-  GPIO_PORTF_DEN_R |= (SW1|SW2); // 7) enable digital I/O on PF0 and PF4
-}
-
-//------------Board_Input------------
-// Read and return the status of the switches.
-// Input: none
-// Output: 0x01 if only Switch 1 is pressed
-//         0x10 if only Switch 2 is pressed
-//         0x00 if both switches are pressed
-//         0x11 if no switches are pressed
-uint32_t Board_Input(void){
-  return SWITCHES;
-}
-
-// Program 2.9 from Volume 2
-#define PB1 (*((volatile uint32_t *)0x40005008))
-//------------Switch_Init3------------
-// Initialize GPIO Port B bit 1 for input
-// Input: none
-// Output: none
-void Switch_Init3(void){
-  SYSCTL_RCGCGPIO_R |= 0x02;        // 1) activate clock for Port B
-  while((SYSCTL_PRGPIO_R&0x02) == 0){};// ready?
-  GPIO_PORTB_DIR_R &= ~0x02;        // PB1 is an input
-  GPIO_PORTB_AFSEL_R &= ~0x02;      // regular port function
-  GPIO_PORTB_AMSEL_R &= ~0x02;      // disable analog on PB1 
-  GPIO_PORTB_PCTL_R &= ~0x000000F0; // PCTL GPIO on PB1 
-  GPIO_PORTB_DEN_R |= 0x02;         // PB3-0 enabled as a digital port
-}
-//------------Switch_Input3------------
-// Read and return the status of GPIO Port B bit 1 
-// Input: none
-// Output: 0x02 if PB1 is high
-//         0x00 if PB1 is low
-uint32_t Switch_Input3(void){ 
-  return PB1;      // 0x02 if pressed, 0x00 if not pressed
-}
-
+#include "Switch.h"
+#include "Time.h"
 #define DELAY10MS 160000
-#define DELAY10US 160
-//------------Switch_Debounce------------
-// Read and return the status of the switch 
-// Input: none
-// Output: 0x02 if PB1 is high
-//         0x00 if PB1 is low
-// debounces switch
-uint32_t Switch_Debounce(void){
-uint32_t in,old,time; 
-  time = 1000; // 10 ms
-  old = Switch_Input();
-  while(time){
-    SysTick_Wait(DELAY10US); // 10us
-    in = Switch_Input();
-    if(in == old){
-      time--; // same value 
-    }else{
-      time = 1000;  // different
-      old = in;
-    }
-  }
-  return old;
+bool debounce[4];
+
+void Buttons_Init(void) {
+	SYSCTL_RCGCGPIO_R |= 0x10;        // 1) activate clock for Port E
+  while((SYSCTL_PRGPIO_R&0x10)==0); // allow time for clock to start
+                                    // 2) no need to unlock PE0-3
+  GPIO_PORTE_PCTL_R &= ~0x000F0F00; // 3) regular GPIO
+  GPIO_PORTE_AMSEL_R &= ~0x0F;      // 4) disable analog function on PE0-3
+                                    // 5) no pullup for external switches
+  GPIO_PORTE_DIR_R &= ~0x0F;        // 5) set direction to output
+  GPIO_PORTE_AFSEL_R &= ~0x0F;      // 6) regular port function
+  GPIO_PORTE_DEN_R |= 0x0F;         // 7) enable digital port
+	
+	Timer1_Init();
 }
 
-//------------Switch_Debounce------------
-// wait for the switch to be touched 
-// Input: none
-// Output: none
-// debounces switch
-void Switch_WaitForTouch(void){
-// wait for release
-  while(Switch_Input()){};
-  SysTick_Wait(DELAY10MS); // 10ms
-// wait for touch
-  while(Switch_Input()==0){};
-  SysTick_Wait(800000); // 10ms
+void Buttons_Input(void) {
+	int32_t data = GPIO_PORTE_DATA_R;
+	
+	if((data & 0x01) != 0)
+		debounce[0] = (data & 0x01)*DELAY10MS;
+	if((data & 0x02) != 0)
+		debounce[1] = ((data & 0x2) >> 1)*DELAY10MS;
+	if((data & 0x04) != 0)
+		debounce[2] = ((data & 0x4) >> 2)*DELAY10MS;
+	if((data & 0x08) != 0)
+		debounce[3] = ((data & 0x8) >> 3)*DELAY10MS;
 }
-  
+
+int Buttons_Pressed(uint32_t button) {
+	if(button == 0){ return 0;}
+	if(button == 1){ return 1;}
+	if(button == 2){ return 2;}
+	if(button == 3){ return 3;}
+	else return -1;
+}
+
+void Buttons_Handler(void) {
+	Buttons_Input();
+	for(uint32_t i = 0; i < 4; i += 1) {
+		if(debounce[i] > 0) {
+			debounce[i] -= 1;
+			if(debounce[i] == 0) {
+				Buttons_Pressed(i);
+			}
+		}
+	}
+}
--- switch.d
+++ switch.d
@@ -1,4 +1,5 @@
 .\switch.o: Switch.c
 .\switch.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\switch.o: ../inc/tm4c123gh6pm.h
-.\switch.o: SysTick.h
+.\switch.o: Switch.h
+.\switch.o: Time.h
